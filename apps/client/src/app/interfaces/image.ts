36a3dcd439bca89453aadbe4eb8d5cea581798f1
Albert Ortega <trebla.og46@gmail.com>
2024-01-05 21:20:04 +0100

    Image service and interface

---
+++ apps/client/src/app/interfaces/image.ts
@@ -0,0 +1,5 @@
+export interface Image {
+  id: number
+  label: string
+  url: string
+}
---
+++ apps/client/src/app/services/image.service.ts
@@ -0,0 +1,33 @@
+import {Injectable, inject} from '@angular/core'
+import {Observable, catchError, map, of} from 'rxjs'
+import {Image} from '../interfaces/image'
+import {HttpClient} from '@angular/common/http'
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ImageService {
+  private baseUrl: string = ''
+
+  http = inject(HttpClient)
+
+  getAllImages(): Observable<Image[]> {
+    return this.http.get<Image[]>(`${this.baseUrl}/images`)
+  }
+
+  addImage(image: Image): Observable<Image> {
+    return this.http.post<Image>(`${this.baseUrl}/images`, image)
+  }
+
+  deleteImageById(id: number): Observable<boolean> {
+    if (!id) throw Error('Image id is required')
+    return this.http.delete(`${this.baseUrl}/images/${id}`).pipe(
+      map((res) => true),
+      catchError((err) => of(false)),
+    )
+  }
+
+  getImageByLabel(label: string): Observable<Image> | undefined {
+    return of()
+  }
+}
