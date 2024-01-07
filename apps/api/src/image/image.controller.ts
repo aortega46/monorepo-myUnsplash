800ccaeb74dc5c21fe1f9517cef68a594dba9169
Albert Ortega <trebla.og46@gmail.com>
2024-01-07 17:57:55 +0100

    (api) Check URL valid images

--- apps/api/src/image/image.controller.ts
+++ apps/api/src/image/image.controller.ts
@@ -6,18 +6,38 @@ import {
   Param,
   Delete,
   Query,
+  BadRequestException,
+  InternalServerErrorException,
 } from '@nestjs/common'
 import { ImageService } from './image.service'
 import { CreateImageDto } from './dto/create-image.dto'
-import { query } from 'express'
 
 @Controller('images')
 export class ImageController {
   constructor(private readonly imageService: ImageService) {}
 
   @Post()
-  create(@Body() createImageDto: CreateImageDto) {
-    return this.imageService.create(createImageDto)
+  async create(@Body() createImageDto: CreateImageDto) {
+    const newUrl = createImageDto.url.includes('http')
+      ? createImageDto.url
+      : `https://${createImageDto.url}`
+
+    const newCreateImageDto = { ...createImageDto, url: newUrl }
+
+    try {
+      const image = await fetch(newUrl).catch(() => {
+        throw new BadRequestException('URL not valid')
+      })
+
+      const buff = await image.blob()
+      const isImage = buff.type.startsWith('image/')
+
+      if (isImage) return this.imageService.create(newCreateImageDto)
+      else throw new BadRequestException('URL does not conatain an image')
+    } catch (error) {
+      if (error.status) throw error
+      throw new InternalServerErrorException('Something went wrong')
+    }
   }
 
   @Get()
